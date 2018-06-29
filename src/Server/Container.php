ae26cee475101476a125700dc46394244de95040
Allen Luo <movoin@gmail.com>
2018-06-29 18:05:36 +0800

    update

---
+++ src/Server/Container.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * This file is part of the One package.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @package     One\Server
+ * @author      Allen Luo <movoin@gmail.com>
+ * @since       0.1
+ */
+
+namespace One\Server;
+
+class Container
+{
+
+}
---
+++ src/Swoole/Server.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * This file is part of the One package.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @package     One\Swoole
+ * @author      Allen Luo <movoin@gmail.com>
+ * @since       0.1
+ */
+
+namespace One\Swoole;
+
