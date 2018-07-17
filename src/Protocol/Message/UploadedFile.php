0d8e1ea333a394089209cb413ca127c577a95270
Allen Luo <movoin@gmail.com>
2018-07-18 00:05:17 +0800

    update

---
+++ src/Protocol/Message/UploadedFile.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * This file is part of the One package.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @package     One\Protocol\Message
+ * @author      Allen Luo <movoin@gmail.com>
+ * @since       0.1
+ */
+
+namespace One\Protocol\Message;
+
+use RuntimeException;
+use InvalidArgumentException;
+use Psr\Http\Message\StreamInterface;
+use Psr\Http\Message\UploadedFileInterface;
+
+class UploadedFile implements UploadedFileInterface
+{
+}
