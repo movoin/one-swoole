d68c8f632199a530ef54da525745354950fb161c
Allen Luo <movoin@gmail.com>
2018-07-28 18:13:20 +0800

    update

--- src/Run.php
+++ src/Run.php
@@ -12,6 +12,76 @@
 
 namespace One;
 
+use Psr\Log\LoggerInterface;
+use Symfony\Component\Process\Process;
+use Symfony\Component\Process\Exception\ProcessFailedException;
+
 final class Run
 {
+    /**
+     * 日志对象
+     *
+     * @var \Psr\Log\LoggerInterface
+     */
+    private static $logger;
+
+    /**
+     * 运行 Shell
+     *
+     * @param array|string  $sh
+     * @param string|null   $cwd
+     * @param int           $timeout
+     * @param bool          $async
+     *
+     * @return mixed
+     * @throws \Symfony\Component\Process\Exception\ProcessFailedException
+     */
+    public static function shell($sh, $cwd = null, int $timeout = 60, bool $async = false)
+    {
+        $process = new Process($sh, $cwd, null, null, $timeout);
+
+        if ($async) {
+            $process->start();
+            $process->wait(function ($type, $buffer) {
+                if (Process::ERR === $type) {
+                    static::log('error', $buffer);
+                }
+            });
+        } else {
+            $process->run();
+
+            if (! $process->isSuccessful()) {
+                static::log('error', $process->getErrorOutput());
+                throw new ProcessFailedException($process);
+            }
+
+            return $process->getOutput();
+        }
+
+        unset($process);
+    }
+
+    /**
+     * 设置日志对象
+     *
+     * @param \Psr\Log\LoggerInterface $logger
+     */
+    public static function setLogger(LoggerInterface $logger)
+    {
+        static::$logger = $logger;
+    }
+
+    /**
+     * 写入日志
+     *
+     * @param string $level
+     * @param string $message
+     * @param array  $context
+     */
+    private static function log(string $level, string $message, array $context = [])
+    {
+        if (static::$logger !== null) {
+            static::$logger->$level($message, $context);
+        }
+    }
 }
